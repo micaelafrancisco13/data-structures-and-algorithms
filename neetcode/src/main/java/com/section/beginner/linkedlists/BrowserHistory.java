229ea02fc53a9d4553a4339440de7e2a25f3e293
Ela <micaelafrancisco13@outlook.com>
2025-08-23 19:23:00 +0800

    fix: maven pom error

---
+++ neetcode/src/main/java/com/section/beginner/linkedlists/BrowserHistory.java
@@ -0,0 +1,19 @@
+package com.section.beginner.linkedlists;
+
+public class BrowserHistory {
+    public BrowserHistory(String homepage) {
+
+    }
+
+    public void visit(String url) {
+
+    }
+
+    public String back(int steps) {
+        return "";
+    }
+
+    public String forward(int steps) {
+        return "";
+    }
+}
